57af89d5083d9d2a783d44ee816f4a6723e56900
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2025-11-10 18:49:32 +0200

    login and regist access

--- backend/src/user/strategies/jwt.strategies.ts
+++ backend/src/user/strategies/jwt.strategies.ts
@@ -3,15 +3,41 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayloadType, UserService } from '../user.service';
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+
+// // @Injectable()
+// // export class JwtStrategy extends PassportStrategy(Strategy) {
+// //   constructor(
+// //     private readonly configService: ConfigService,
+// //     private readonly authService: UserService,
+// //   ) {
+// //     super({
+// //       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+// //       ignoreExpiration: false,
+// //       secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+// //       algorithms: ['HS256'],
+// //     });
+// //   }
+
+// //   async validate(payload: JwtPayloadType) {
+// //     return await this.authService.validate(payload.id);
+// //   }
+// // }
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtCookieStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-cookie',
+) {
   constructor(
     private readonly configService: ConfigService,
     private readonly authService: UserService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        return req?.cookies?.accessToken; // беремо токен з кукі
+      },
       ignoreExpiration: false,
       secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
       algorithms: ['HS256'],
@@ -19,6 +45,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayloadType) {
-    return await this.authService.validate(payload.id);
+    return this.authService.validate(payload.id);
   }
 }
---
+++ frontend/src/services/users.ts
@@ -0,0 +1,21 @@
+import {
+  userLogin,
+  userLoginData,
+  userRegister,
+  userRegisterData,
+} from "../types/users";
+import { instance } from "../api/api";
+
+export const registerUser = async (
+  userRegisterData: userRegister
+): Promise<userRegisterData> => {
+  const res = await instance.post("auth/register", userRegisterData);
+  return res.data;
+};
+
+export const loginUser = async (
+  userLoginData: userLogin
+): Promise<userLoginData> => {
+  const resLog = await instance.post("auth/login", userLoginData);
+  return resLog.data;
+};
