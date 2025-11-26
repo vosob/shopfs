0c80bf080f8bf3d24a73e409b18897412839a568
vosob <osobskyi.vlad@gmail.com>
2025-11-26 17:41:38 +0200

    hide eslint err

--- frontend/src/Utils/getTranslatedField.ts
+++ frontend/src/Utils/getTranslatedField.ts
@@ -1,7 +1,16 @@
+// export function getTranslatedField<T, K extends string, L extends string>(
+//   obj: T,
+//   field: K,
+//   lang: L
+// ): T extends Record<`${K}_${L}`, infer R> ? R : never {
+//   return obj[`${field}_${lang}` as `${K}_${L}`];
+// }
+
 export function getTranslatedField<T, K extends string, L extends string>(
   obj: T,
   field: K,
   lang: L
 ): T extends Record<`${K}_${L}`, infer R> ? R : never {
-  return obj[`${field}_${lang}` as `${K}_${L}`];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return obj[`${field}_${lang}` as keyof T] as any;
 }
