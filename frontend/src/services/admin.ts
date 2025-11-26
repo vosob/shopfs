902ec1182a01906953fc248073d00ba854eda7a7
vosob <osobskyi.vlad@gmail.com>
2025-11-26 17:45:57 +0200

    hide admin component

--- frontend/src/services/admin.ts
+++ frontend/src/services/admin.ts
@@ -1,12 +1,12 @@
-import { BouquetPayload } from "../pages/AdminPage/AdminPage";
-import { instance } from "./axiosInstans";
+// import { BouquetPayload } from "../pages/AdminPage/AdminPage";
+// import { instance } from "./axiosInstans";
 
-export const postBouquet = async (data: BouquetPayload) => {
-  const res = await instance.post("bouquet/add", data);
-  return res.data;
-};
+// export const postBouquet = async (data: BouquetPayload) => {
+//   const res = await instance.post("bouquet/add", data);
+//   return res.data;
+// };
 
-export const getFlowers = async () => {
-  const res = await instance.post("flower");
-  return res.data;
-};
+// export const getFlowers = async () => {
+//   const res = await instance.post("flower");
+//   return res.data;
+// };
